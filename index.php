8b9db666c5f90e23d347b43b3be4f8a520f216bb
Br14n937 <105537730+AlexandroFebrian@users.noreply.github.com>
2022-11-08 17:10:21 +0700

    test img

--- index.php
+++ index.php
@@ -48,16 +48,16 @@
             </div>
         </nav>
 
-        <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                <img src="..." class="d-block w-100" alt="...">
+        <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel" style="margin-top: 79px;">
+            <div class="carousel-inner" style="height: 800px;">
+                <div class="carousel-item active" data-bs-interval="5000">
+                <img src="img/fot1.png" class="d-block w-100" alt="..." style="height: 100%;">
                 </div>
-                <div class="carousel-item">
-                <img src="..." class="d-block w-100" alt="...">
+                <div class="carousel-item" data-bs-interval="5000">
+                <img src="img/fot2.png" class="d-block w-100" alt="..." style="height: 100%;">
                 </div>
-                <div class="carousel-item">
-                <img src="..." class="d-block w-100" alt="...">
+                <div class="carousel-item" data-bs-interval="5000">
+                <img src="img/fot3.png" class="d-block w-100" alt="..." style="height: 100%;">
                 </div>
             </div>
         </div>
