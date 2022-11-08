202dbdd9b1c804e58d825ad15bb8e3c93185bbd6
Br14n937 <105537730+AlexandroFebrian@users.noreply.github.com>
2022-11-08 12:24:21 +0700

    test cetak gambar

---
+++ Connection.php
@@ -0,0 +1,16 @@
+<?php
+    session_start();
+
+    //Parameter untuk mysqli_connection -> host, user, password, db_name
+    $conn = mysqli_connect("localhost", "root", "", "db_toko_kacamata");
+
+    //Untuk melakukan check apabila ada error atau tidak
+    if(mysqli_connect_errno()){
+        echo mysqli_error($conn);
+    }
+
+    if(isset($_SESSION["message"])){
+        echo "<script>alert('$_SESSION[message]')</script>";
+        unset($_SESSION["message"]);
+    }
+?>
---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+    require_once("Connection.php");
+
+    $result = mysqli_query($conn, "SELECT * FROM kacamata, color WHERE kc_id = co_kc_id")
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="">
+        <?php
+            while($row = mysqli_fetch_array($result)){
+                $kc_id = $row["kc_id"];
+                $co_id = $row["co_id"];
+                $co_link = $row["co_link"];
+        ?>
+                <div style="width: 300px">
+                    <img src='<?= $co_link ?>' style="width: 100%">
+                    <h3><?= $kc_id ?></h3>
+                    <h4><?= $co_id ?></h4>
+                </div>
+        <?php
+            }
+        ?>
+    </form>
+</body>
+</html>
