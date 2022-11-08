a4254f4e042bc71162d308141a461160ec3b52ac
Br14n937 <105537730+AlexandroFebrian@users.noreply.github.com>
2022-11-08 17:21:59 +0700

    a

--- index.php
+++ index.php
@@ -10,13 +10,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- <link rel="stylesheet" href="css/bootstrap.min.css"> -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 </head>
 <body>
     <form action="">
-        <nav class="navbar navbar-expand-lg bg-danger p-3 position-fixed position-absolute top-0 w-100">
+        <nav class="navbar navbar-expand-lg bg-white p-3 position-fixed position-absolute top-0 w-100">
             <div class="container-fluid">
-                <a class="navbar-brand text-white" href="index.php"><h4>Optik Primadona</h4></a>
+                <a class="navbar-brand" href="index.php"><h4>Optik Primadona</h4></a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
                 </button>
@@ -48,7 +49,7 @@
             </div>
         </nav>
 
-        <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel" style="margin-top: 79px;">
+        <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel" style="margin-top: 79px; z-index: -1;">
             <div class="carousel-inner" style="height: 800px;">
                 <div class="carousel-item active" data-bs-interval="5000">
                 <img src="img/fot1.png" class="d-block w-100" alt="..." style="height: 100%;">
@@ -93,7 +94,7 @@
 
 
 
-    <script src="script/bootstrap.min.js"></script>
+    <script src="script/bootstrap.bundle.min.js"></script>
     <script src="script/jquery-3.6.1.min.js"></script>
 </body>
 </html>
