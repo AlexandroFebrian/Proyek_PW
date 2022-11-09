753eb39885e8dc488df63a751b30af1670284a0d
Br14n937 <105537730+AlexandroFebrian@users.noreply.github.com>
2022-11-09 20:41:50 +0700

    ivan

---
+++ detail.php
@@ -0,0 +1,45 @@
+<?php
+    require_once("Connection.php");
+
+    $kc_id = "";
+    if (isset($_GET["id"])) {
+        $kc_id = $_GET["id"];
+    }
+
+    if ($kc_id != "") {
+        $result = mysqli_fetch_array(mysqli_query($conn, "SELECT * FROM kacamata JOIN color ON kc_id = co_kc_id JOIN brand ON kc_br_id = br_id WHERE kc_id = '$kc_id'"));
+        $kc_id = $result["kc_id"];
+        $kc_price = $result["kc_price"];
+        $co_id = $result["co_id"];
+        $co_link = $result["co_link"];
+        $br_name = $result["br_name"];
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        if ($kc_id != "") {
+    ?>
+            <div class="col text-center me-4 mb-5">
+                <div class="card" style="width: 18rem; border: none;">
+                    <img src='<?= $co_link ?>' class="card-img-top">
+                    <div class="card-body">
+                        <h4 class="card-title"><?= $br_name ?></h4>
+                        <p class="card-text fs-5"><?= "SKU-" . $co_id ?>
+                        <br><?= "Rp " . number_format($kc_price, 0, "", ",") ?></p>
+                    </div>
+                </div>
+            </div>
+    <?php
+        }
+    ?>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,7 +1,22 @@
 <?php
     require_once("Connection.php");
 
-    $result = mysqli_query($conn, "SELECT * FROM kacamata, color, brand WHERE kc_id = co_kc_id AND kc_br_id = br_id")
+    $page = 1;
+    if (isset($_GET["page"])) {
+        $page = $_GET["page"];
+    }
+
+    if ($page < 1) {
+        header("Location: index.php?page=1");
+    } else if ($page > 54) {
+        header("Location: index.php?page=54");
+    }
+
+    $result = [];
+    $tempresult = mysqli_query($conn, "SELECT * FROM kacamata JOIN color ON kc_id = co_kc_id JOIN brand ON kc_br_id = br_id");
+    while ($row = mysqli_fetch_array($tempresult)) {
+        $result[] = $row;
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -13,9 +28,9 @@
     <!-- <link rel="stylesheet" href="css/bootstrap.min.css"> -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 </head>
-<body style="height: 4000px">
-    <form action="">
-        <nav class="navbar navbar-expand-lg bg-white p-3 position-fixed position-absolute top-0 w-100">
+<body>
+    <form>
+        <nav class="navbar navbar-expand-lg bg-white p-3 position-fixed position-absolute top-0 w-100" style="z-index: 5;">
             <div class="container-fluid">
                 <a class="navbar-brand" href="index.php"><h4>Optik Primadona</h4></a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -24,7 +39,10 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Pencarian produk</a>
+                        <a class="nav-link active" aria-current="page" href="#">Pencarian produk</a>
+                        <ul>
+                            
+                        </ul>
                     </li>
                     <li class="nav-item">
                     <a class="nav-link" href="#">Link</a>
@@ -49,7 +67,7 @@
             </div>
         </nav>
 
-        <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel" style="margin-top: 79px; z-index: -1;">
+        <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel" style="margin-top: 79px;">
             <div class="carousel-inner" style="height: 800px;">
                 <div class="carousel-item active" data-bs-interval="5000">
                 <img src="img/fot1.png" class="d-block w-100" alt="..." style="height: 100%;">
@@ -63,32 +81,89 @@
             </div>
         </div>
 
-        <!-- <div class="container-xxl" style="margin-top: 100px;">
+        <div class="container-xxl mt-5">
             <div class="row">
-                <div class="col-3">
-                    <h1>test</h1>
-                </div>
-                <div class="col-9">
-                    <?php
-                        while($row = mysqli_fetch_array($result)){
-                            $kc_id = $row["kc_id"];
-                            $co_id = $row["co_id"];
-                            $co_link = $row["co_link"];
-                            $br_name = $row["br_name"]
-                    ?>
-                            <div style="width: 300px">
-                                <img src='<?= $co_link ?>' style="width: 100%">
-                                <h3><?= $br_name ?></h3>
-                                <h4><?= $kc_id ?></h4>
-                                <h5><?= $co_id ?></h5>
-                            </div>
-                    <?php
-                        }
-                    ?>
-
+                <div class="col">
+                    <h1>Popoular Brand</h1>
                 </div>
             </div>
-        </div> -->
+            
+            <div class="row">
+                <?php
+                    for ($i = ($page - 1) * 30; $i < $page * 30; $i++) {
+                        if (isset($result[$i])) {
+                            $kc_id = $result[$i]["kc_id"];
+                            $kc_price = $result[$i]["kc_price"];
+                            $co_id = $result[$i]["co_id"];
+                            $co_link = $result[$i]["co_link"];
+                            $br_name = $result[$i]["br_name"];
+                ?>
+                            <div class="col text-center me-4 mb-5">
+                                <a href='<?= "detail.php?id=" . $kc_id ?>' class="text-black text-decoration-none">
+                                    <div class="card" style="width: 18rem; border: none;">
+                                        <img src='<?= $co_link ?>' class="card-img-top">
+                                        <div class="card-body">
+                                            <h4 class="card-title"><?= $br_name ?></h4>
+                                            <p class="card-text fs-5"><?= "SKU-" . $co_id ?>
+                                            <br><?= "Rp " . number_format($kc_price, 0, "", ",") ?></p>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                <?php
+                        }
+                    }
+                ?>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-4">
+                    <ul class="pagination">
+                        <li class="page-item">
+                        <a class="page-link" href='index.php?page=<?php if ($page - 1 > 0) echo $page - 1; else echo "1";?>' aria-label="Previous">
+                            <span aria-hidden="true">&laquo;</span>
+                        </a>
+                        </li>
+                        <?php
+                            if ($page < 5) {
+                        ?>
+                            <li class="page-item"><a class="page-link" href="index.php?page=1">1</a></li>
+                            <li class="page-item"><a class="page-link" href="index.php?page=2">2</a></li>
+                            <li class="page-item"><a class="page-link" href="index.php?page=3">3</a></li>
+                            <li class="page-item"><a class="page-link" href="index.php?page=4">4</a></li>
+                            <li class="page-item"><a class="page-link" href="index.php?page=5">5</a></li>
+                            <li class="page-item"><a class="page-link">...</a></li>
+                            <li class="page-item"><a class="page-link" href="index.php?page=54">54</a></li>
+                        <?php
+                            } else if ($page > 4 && $page < 51) {
+                        ?>
+                            <li class="page-item"><a class="page-link" href="index.php?page=1">1</a></li>
+                            <li class="page-item"><a class="page-link">...</a></li>
+                            <li class="page-item"><a class="page-link" href='index.php?page=<?=$page - 1?>'><?= $page - 1 ?></a></li>
+                            <li class="page-item"><a class="page-link" href='index.php?page=<?=$page?>'><?= $page ?></a></li>
+                            <li class="page-item"><a class="page-link" href='index.php?page=<?=$page + 1?>'><?= $page + 1 ?></a></li>
+                            <li class="page-item"><a class="page-link">...</a></li>
+                            <li class="page-item"><a class="page-link" href="index.php?page=54">54</a></li>
+                        <?php
+                            } else {
+                        ?>
+                            <li class="page-item"><a class="page-link" href="index.php?page=1">1</a></li>
+                            <li class="page-item"><a class="page-link">...</a></li>
+                            <li class="page-item"><a class="page-link" href="index.php?page=50">50</a></li>
+                            <li class="page-item"><a class="page-link" href="index.php?page=51">51</a></li>
+                            <li class="page-item"><a class="page-link" href="index.php?page=52">52</a></li>
+                            <li class="page-item"><a class="page-link" href="index.php?page=53">53</a></li>
+                            <li class="page-item"><a class="page-link" href="index.php?page=54">54</a></li>
+                        <?php
+                            }
+                        ?>
+                        <a class="page-link" href='index.php?page=<?php if ($page + 1 < 54) echo $page + 1; else echo "54";?>' aria-label="Next">
+                            <span aria-hidden="true">&raquo;</span>
+                        </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     </form>
 
 
