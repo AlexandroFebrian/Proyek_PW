10264f6a8e6beb5705f43e1a62db3ca32a1c107d
Ivan Susanto <105846193+ivansusanto@users.noreply.github.com>
2022-12-01 23:43:15 +0700

    Update adminlogin.php

--- adminlogin.php
+++ adminlogin.php
@@ -6,7 +6,7 @@
     if(isset($_POST["login"])){
         $password = $_POST["password"];
 
-        if($password === "2s*MkL]bh$]q"){
+        if(password_verify($password, '$2a$12$D2wkqnqLVPD1kYXeMKAcYuTWEqpO6f76sil0VJCXD7abXd7rmJgdW')){
             $_SESSION["admin"] = true;
             header("Location: admin.php");
         }
